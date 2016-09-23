75be6c7d4d5fdcfb4e221c00d5db964be9352f1a
gowrisankar Gnanasekaran <gowrisankar Gnanasekaran>
2016-09-23 11:07:46 +0530

    Port Settings Changed

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func helloHandler(res http.ResponseWriter, req *http.Request) {
@@ -29,10 +30,22 @@ func helloHandler(res http.ResponseWriter, req *http.Request) {
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Go web app powered by Docker")
 }
+
+// GetPort -- get the Port from the Dynamic environment
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	if port == "" {
+		port = "4747"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+
+	fmt.Println("Running Port is" + port)
+	return ":" + port
+}
 func main() {
 	http.HandleFunc("/", defaultHandler)
 	http.HandleFunc("/hello", helloHandler)
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(GetPort(), nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 		return
